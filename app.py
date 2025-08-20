24b205c6671f923296dd4e4c9d3cfcbe2bf89916
Preethi98765 <2020preethis@gmail.com>
2025-08-20 15:30:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+categories = []  # temporary storage
 
 @app.route('/')
 def home():
     return render_template("index.html")
+
+@app.route('/categories', methods=['GET', 'POST'])
+def manage_categories():
+    if request.method == 'POST':
+        category_name = request.form['category_name']
+        categories.append(category_name)
+    return render_template("categories.html", categories=categories)
 
 if __name__ == '__main__':
     app.run(debug=True)
