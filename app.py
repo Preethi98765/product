28e654ffc3b435c0698a1226785eeea0deae2e19
Preethi98765 <2020preethis@gmail.com>
2025-08-20 15:35:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,31 @@ def manage_attributes():
         data_type = request.form['data_type']
         attributes.append({"name": attribute_name, "type": data_type})
     return render_template("attributes.html", attributes=attributes)
+products = []  # temporary storage
+
+@app.route('/products', methods=['GET', 'POST'])
+def manage_products():
+    if request.method == 'POST':
+        product_name = request.form['product_name']
+        category = request.form['category']
+
+        # Collect attributes dynamically
+        product_attributes = {}
+        for attr in attributes:
+            field_name = f"attr_{attr['name']}"
+            product_attributes[attr['name']] = request.form.get(field_name)
+
+        products.append({
+            "name": product_name,
+            "category": category,
+            "attributes": product_attributes
+        })
+
+    return render_template("products.html", 
+                           categories=categories, 
+                           attributes=attributes, 
+                           products=products)
+
 
 
 if __name__ == '__main__':
